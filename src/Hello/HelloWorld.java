001a920e1831a0d1b427383e00a678ae805f07df
Brian Lim <bterrific2008@gmail.com>
2016-10-14 11:13:43 -0700

    inital project version

---
+++ src/Hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package Hello;
+
+public class HelloWorld {
+	
+	
+	
+	public static void main(String [ ] args){
+	System.out.println("Hello World");
+	System.out.println("Rohan");
+	System.out.print("Rohan");
+	}
+	
+	
+	
+	
+}
